10a9f32d99dd6449795403903a033bc3c130a030
Chris Price <chrispr@seas.upenn.edu>
2021-12-06 23:41:09 -0500

    Update base_trainer.py

--- ocpmodels/trainers/base_trainer.py
+++ ocpmodels/trainers/base_trainer.py
@@ -687,15 +687,15 @@ class BaseTrainer(ABC):
                     f"{self.name}_{results_file}_{i}.npz",
                 )
                 rank_results = np.load(rank_path, allow_pickle=True)
-                # gather_results["ids"].extend(rank_results["ids"])
+                gather_results["ads_sid"].extend(rank_results["ads_sid"])
                 for key in keys:
                     gather_results[key].extend(rank_results[key])
                 os.remove(rank_path)
 
             # Because of how distributed sampler works, some system ids
             # might be repeated to make no. of samples even across GPUs.
-            _, idx = np.unique(gather_results["ids"], return_index=True)
-            gather_results["ids"] = np.array(gather_results["ids"])[idx]
+            _, idx = np.unique(gather_results["ads_sid"], return_index=True)
+            gather_results["ads_sid"] = np.array(gather_results["ads_sid"])[idx]
             for k in keys:
                 if k == "forces":
                     gather_results[k] = np.concatenate(
