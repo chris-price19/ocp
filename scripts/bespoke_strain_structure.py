3e08cc37ed77b5b92e09972e46b09217884819b8
Chris Price <chrispr@seas.upenn.edu>
2021-12-18 01:14:45 -0500

    Update bespoke_strain_structure.py

--- scripts/bespoke_strain_structure.py
+++ scripts/bespoke_strain_structure.py
@@ -22,6 +22,162 @@ from pymatgen.analysis.adsorption import reorient_z
 from ase.io import read, write
 from ase.calculators.vasp import Vasp
 
+VASP_FLAGS = {'ibrion': 2,
+          'nsw': 600,
+          # 'nelm': 5,  # turn this off!
+          'isif': 0,
+          'isym': 0,
+          'lreal': 'Auto',
+          'ediffg': -0.03,
+          'symprec': 1e-10,
+          'encut': 400,
+          # 'laechg': False,
+          'lwave': False,
+          'lcharg': False,
+          'ncore': 8,
+          'kpar': 2,
+          'gga': 'RP',
+          'pp': 'PBE',
+          'prec': 'Normal',
+          'ediff': 1e-4,
+          'ivdw': 12,
+          'amin': 0.01,
+          'lcorr': True,
+          'nsim': 16,
+          'ismear': 1,
+          # 'nblock': 1,
+          # 'gga_compat': False
+          # 'xc': 'PBE'
+          }
+
+class DeltaTemplate(Template):
+    delimiter = "%"
+
+def strfdelta(tdelta, fmt):
+    d = {"D": tdelta.days}
+    hours, rem = divmod(tdelta.seconds, 3600)
+    hours += tdelta.days*24
+    minutes, seconds = divmod(rem, 60)
+    d["H"] = '{:02d}'.format(hours)
+    d["M"] = '{:02d}'.format(minutes)
+    d["S"] = '{:02d}'.format(seconds)
+    t = DeltaTemplate(fmt)
+    return t.substitute(**d)
+
+def calculate_surface_k_points(atoms):
+    '''
+    For surface calculations, it's a good practice to calculate the k-point
+    mesh given the unit cell size. We do that on-the-spot here.
+    Arg:
+        atoms   `ase.Atoms` object of the structure we want to relax
+    Returns:
+        k_pts   A 3-tuple of integers indicating the k-point mesh to use
+    '''
+    cell = atoms.get_cell()
+    order = np.inf
+    a0 = np.linalg.norm(cell[0], ord=order)
+    b0 = np.linalg.norm(cell[1], ord=order)
+    multiplier = 40
+    k_pts = (max(1, int(round(multiplier/a0))),
+             max(1, int(round(multiplier/b0))),
+             1)
+    return k_pts
+
+def run_slab_calc(basedir, atoms, data):
+
+    os.chdir(basedir)
+    cwd = os.getcwd()
+
+    newdir = str(data['ads_sid']) + '.' + str(data['strain_id']) + '/slab'
+    os.makedirs(newdir, exist_ok=True)
+    os.chdir(newdir)
+
+    ## check for running status.
+    mod_time = sorted([os.path.getmtime(f) for f in os.listdir()])
+    if len(mod_time) > 0:
+        last_time = mod_time[-1]
+        if round(time.time()) - last_time < 60*5: # if the most recent file has been updated within the last 5 minutes.
+            return False, data
+
+    os.system('echo $SLURM_JOBID >> jobfile.out')
+    ## delete adsorbate atoms
+    atoms = atoms.copy()    
+    keep = [0,1]  # 0 = subsurface atoms, 1 = surface atoms, 2 = adsorbate atoms
+    icount = 0
+    tags = data['tags'].copy()
+    while not np.array_equal(np.unique(tags), np.unique(keep)):
+        if tags[icount] in keep:
+            icount += 1
+        else:
+            atoms.pop(icount)
+            tags = np.delete(tags, icount)
+    
+    ## check for CONTCAR / continuation
+    if os.path.exists('CONTCAR'):
+        if os.path.getsize('CONTCAR') > 0:
+            atoms = ase.io.read('CONTCAR', format='vasp')
+
+    vasp_flags = VASP_FLAGS.copy()
+    vasp_flags['kpts'] = calculate_surface_k_points(atoms)
+    calc = Vasp(setups='recommended', **vasp_flags)
+    atoms.calc = calc
+
+    ## output from vasp goes to vasp.out
+    try:
+        energy = atoms.get_potential_energy()
+        data['slab_E'] = energy
+    except Exception as err:
+        data['error'] = str(err) + '_slab'
+        print(str(err))
+
+    # calc.write_input(atoms)
+    
+    os.chdir(basedir)
+
+    return True, data
+
+def run_cat_calc(basedir, atoms, data):
+
+    os.chdir(basedir)
+    cwd = os.getcwd()
+
+    newdir = str(data['ads_sid']) + '.' + str(data['strain_id']) + '/ads'
+    os.makedirs(newdir, exist_ok=True)
+    os.chdir(newdir)
+
+    ## check for running status.
+    mod_time = sorted([os.path.getmtime(f) for f in os.listdir()])
+    if len(mod_time) > 0:
+        last_time = mod_time[-1]
+        if round(time.time()) - last_time < 60*5: # if the most recent file has been updated within the last 5 minutes.
+            return False, atoms, data
+
+    os.system('echo $SLURM_JOBID >> jobfile.out')
+    ## check for CONTCAR / continuation
+    if os.path.exists('CONTCAR'):
+        if os.path.getsize('CONTCAR') > 0:
+            atoms = ase.io.read('CONTCAR', format='vasp')
+
+    vasp_flags = VASP_FLAGS.copy()
+    vasp_flags['kpts'] = calculate_surface_k_points(atoms)
+    calc = Vasp(setups='recommended', **vasp_flags)
+    atoms.calc = calc
+
+    try:
+        energy = atoms.get_potential_energy()
+        data['ads_E'] = energy
+    except Exception as err:
+        data['error'] = str(err)
+        print(str(err))
+
+    # calc.write_input(atoms)
+    
+    os.chdir(basedir)
+
+    return True, atoms, data
+
+
+
 
 adssid = 1436007
 
@@ -29,10 +185,10 @@ base_datadir = '../../dft/'
 
 existing_strain_ids = os.listdir(base_datadir)
 existing_strain_ids = [i.split('.')[0] for i in existing_strain_ids if i.split('.')[0] == str(adssid)]
+max_strain_ids = len(existing_strain_ids)
 
 adsgroundstate = read(base_datadir + str(adssid) + '.0/ads/CONTCAR')
 slabgroundstate = read(base_datadir + str(adssid) + '.0/slab/CONTCAR')
-
 
 
 straintensor = np.array([[0.99565088, 1.02005077, 0.01211224, ]])
@@ -41,5 +197,18 @@ straintensor = np.array([[0.99565088, 1.02005077, 0.01211224, ]])
 
 strain_tensor_list = generate_strain_tensors(1, man_override=straintensor)
 
-print(strain_tensor_list[0].eps)
-print(strain_tensor_list[1].eps)
+adsgroundstate = strain_atoms(adsgroundstate, strain_tensor_list[-1])
+slabgroundstate = strain_atoms(slabgroundstate, strain_tensor_list[-1])
+
+os.makedirs(str(adssid) + '.' + str(max_strain_ids))
+os.chdir(str(adssid) + '.' + str(max_strain_ids))
+
+# vasp_flags = VASP_FLAGS.copy()
+# vasp_flags['kpts'] = calculate_surface_k_points(atoms)
+# calc = Vasp(setups='recommended', **vasp_flags)
+# atoms.calc = calc
+# energy = atoms.get_potential_energy()
+
+print(os.getcwd())
+
+os.chdir(base_datadir)
