3b38064027e76b1430107c4bda694e27b7b3825b
Chris Price <chrispr@seas.upenn.edu>
2021-12-18 01:15:58 -0500

    Update bespoke_strain_structure.py

--- scripts/bespoke_strain_structure.py
+++ scripts/bespoke_strain_structure.py
@@ -50,19 +50,19 @@ VASP_FLAGS = {'ibrion': 2,
           # 'xc': 'PBE'
           }
 
-class DeltaTemplate(Template):
-    delimiter = "%"
+# class DeltaTemplate(Template):
+#     delimiter = "%"
 
-def strfdelta(tdelta, fmt):
-    d = {"D": tdelta.days}
-    hours, rem = divmod(tdelta.seconds, 3600)
-    hours += tdelta.days*24
-    minutes, seconds = divmod(rem, 60)
-    d["H"] = '{:02d}'.format(hours)
-    d["M"] = '{:02d}'.format(minutes)
-    d["S"] = '{:02d}'.format(seconds)
-    t = DeltaTemplate(fmt)
-    return t.substitute(**d)
+# def strfdelta(tdelta, fmt):
+#     d = {"D": tdelta.days}
+#     hours, rem = divmod(tdelta.seconds, 3600)
+#     hours += tdelta.days*24
+#     minutes, seconds = divmod(rem, 60)
+#     d["H"] = '{:02d}'.format(hours)
+#     d["M"] = '{:02d}'.format(minutes)
+#     d["S"] = '{:02d}'.format(seconds)
+#     t = DeltaTemplate(fmt)
+#     return t.substitute(**d)
 
 def calculate_surface_k_points(atoms):
     '''
